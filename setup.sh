beceb110011e91b679094f501b2aa2854983c6d7
James Kennedy <james@thatkennedy.net>
2018-12-13 08:33:03 -0500

    fix using cpu rather than gpu in env deps build

--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
+# conda for some reason isn't on the path when ssh'ing
 CONDA="/opt/anaconda3/bin/conda"
+# name is taken from the environoment.yml file
 ENV="fastai"
+PYTHON_ENV = /opt/anaconda3/envs/$ENV/bin/python
 
-/opt/anaconda3/bin/conda update conda
+/opt/anaconda3/bin/conda update conda -y
 
 if [ ! -d fastai ] ; then
     git clone https://github.com/fastai/fastai.git
 fi
 
-cd fastai; $CONDA env create -f environment-cpu.yml
+cd fastai; $CONDA env create -f environment.yml
 $CONDA install ipykernel -n $ENV -y
-/opt/anaconda3/envs/$ENV/bin/python -m ipykernel install --user --name myenv --display-name "Python ($ENV)"
+$PYTHON_ENV -m ipykernel install --user --name myenv --display-name "Python ($ENV)"
